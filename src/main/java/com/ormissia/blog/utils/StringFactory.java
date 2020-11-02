6622403b67f75201c707eeacc536ebcd39f9c545
宋奕锟 <ormissia@outlook.com>
2020-11-02 08:51:05 +0800

    动态获取当前绝对路径，作为图片存储使用

--- src/main/java/com/ormissia/blog/utils/StringFactory.java
+++ src/main/java/com/ormissia/blog/utils/StringFactory.java
@@ -1,5 +1,6 @@
 package com.ormissia.blog.utils;
 
+import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -13,11 +14,20 @@ public class StringFactory {
 
     //获取图片路径
     public String getImagePath() {
+        //动态获取当前绝对路径作为相对路径使用
+        String relativePath = getRelativePath();
+
         //图片存储路径，按照年月区分图片路径
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM");
         String thisMonth = sdf.format(new Date());
 
-        String imagePath = "C:/topImage/" + thisMonth;
+        String imagePath = relativePath + "\\image\\top-image\\" + thisMonth;
         return imagePath;
+    }
+
+    //获取当前绝对路径，用于文件操作相关
+    public String getRelativePath() {
+
+        return new File("").getAbsolutePath();
     }
 }
---
+++ src/test/java/com/ormissia/blog/utils/StringFactoryTest.java
@@ -0,0 +1,23 @@
+package com.ormissia.blog.utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @Author 宋奕锟
+ * @Date: Create in 2020/11/2 8:39
+ */
+
+class StringFactoryTest {
+
+    @Test
+    void getImagePath() {
+        System.out.println(new StringFactory().getImagePath());
+    }
+
+    @Test
+    void getRelativePath() {
+        System.out.println(new StringFactory().getRelativePath());
+    }
+}
